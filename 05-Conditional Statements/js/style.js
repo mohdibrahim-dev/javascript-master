0a36a720fa2295b487723150e447eaae9c337418
mdibrahim060 <mohdibrahimpov@gmail.com>
2026-08-09 11:04:22 +0530

    Conditional Statement

---
+++ 05-Conditional Statements/js/style.js
@@ -0,0 +1,134 @@
+// if Statement 
+
+// Defination:  The if statement is used to execute a block of code only when a specified condition is true.
+
+// syntax of if Statement 
+
+// if (condition){
+//     // code to execute
+// }
+
+
+// if 
+// javascript Researved keyword that check the condion .
+// condition 
+// this is an expression that retrun True or false.
+// {}
+// in this curly braces the code if run when condition is true.
+
+// eg: 
+
+let age = 20;
+if(age >= 18){
+    console.log("You are eligible to vote.");
+}
+
+// Code explane:
+//  let age = 20;                  we assign an variable with name age and store it's value 20.
+// if  is an javascript Reserved keyword that check condition.
+// (age >= 18) this is an condion, if condion is true then run the code , if condition is false it code done not run.
+
+
+
+// some example:
+
+// 1. if statement with Number
+let marks = 80;
+if (marks >= 40) {
+    console.log("You passed.")
+}
+// 2. if statement with String
+let userName = "Ali";
+if (userName === "Ali"){
+    console.log("Welcome Ali!");
+}
+
+// 3. if statement with Boolean
+
+let isLoggedIn = true;
+if (isLoggedIn){
+    console.log("Welcome to the Website.");
+}
+
+// 4. Multiple statements inside if
+
+let drivingAge = 20;
+if (drivingAge >= 18) {
+    console.log("You are an adult.");
+    console.log("You can vote.");
+    console.log("You can apply for a driving license.")
+}
+
+
+
+// Real world Examples 
+
+// 1. Login 
+
+let isLogged_In = true;
+if(isLogged_In){
+    console.log("Welcome Back");
+}
+
+// ATM 
+
+let balance = 5000;
+if (balance >= 1000){
+    console.log("Withdrawal Allowed")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// if statement  question ans
+
+
+// Interview Questions
+// Q1. ans: if statement is an block of code that run when condition is true.
+
+// Q2. ans: we use if statemet to check the condition is true or false and run an block of code as response.
+
+// Q3. ans: the syntan of if statement.
+// if(condition){
+// // code to executed.
+// }
+
+// Q4. ans: In if statement if  the conditionis faste nothing will happen .
+
+// Q5. ans: we use logical operators inside if statement because with logical operators because logical operator can check and return true or false.
+// eg:
+// let age = 30;
+// if (age >= 18){
+// console.log("you are adult")
+// Q6. ans: if statement run a block of code when condition is true. Ternary Operator short form of if statement and if else statement.
+
+// Coding Assignment
+
+// Q1. ans:Eligible
+
+// Q2. ans: code not execute. result blank.
+
+// Q3. ans: Welcome
+
+// Q4. ans: code not execute. result blank
+
+// Q5. ans:code not execute. result blank
+
+// Q6. ans: Entry allowed
+
+// Q7. ans: Login successful
+
+// Q8. ans: code not execute. result blank
+
+// Q9. ans: very hot
+
+// Q10. ans: login success
+
